3baed6144b1cce05b8dd590b6645e3f93ac38298
Antony Guinard <antony.guinard@gmail.com>
2018-06-08 20:43:58 +0100

    Starting to draw the cannon

--- octoshark/game.elm
+++ octoshark/game.elm
@@ -1,48 +1,73 @@
 module Hello exposing (..)
 
-import Html exposing (Html, button, div, text, program)
-import Html.Events exposing (onClick)
+-- import AnimationFrame
+import Collage exposing (..)
+import Transform exposing (..)
+import Color exposing (..)
+import Element exposing (..)
+import Html exposing (..)
+import Time exposing (Time)
+
+-- MAIN
+
+
+main : Program Never Board Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
+
 type alias Size =
-    { width: Int
-    , height: Int
+    { width : Int
+    , height : Int
     }
 
-type alias Position = 
-    { x: Int
-    , y: Int
+
+type alias Position =
+    { x : Float
+    , y : Float
     }
+
 
 type alias Direction =
-    Int
+    Float
 
-type alias Shark = 
-    { position: Position
+
+type alias Shark =
+    { position : Position
     }
+
 
 type alias Octopus =
-    { position: Position
-    , direction: Direction
+    { position : Position
+    , direction : Direction
     }
+
 
 type alias Canon =
-    { direction: Direction
-    , position: Position
+    { direction : Direction
+    , position : Position
     }
+
 
 type alias Board =
-    { size: Size
-    , sharks: List Shark
-    , octopuses: List Octopus
-    , canon: Canon
+    { size : Size
+    , sharks : List Shark
+    , octopuses : List Octopus
+    , canon : Canon
     }
 
--- type alias Model = Board
 
+
+-- type alias Model = Board
 -- MESSAGES
 
 
@@ -50,37 +75,26 @@ type Msg
     = Increment Int
 
 
-boardSize: Size
-boardSize = { width = 100, height = 100 }
+boardSize : Size
+boardSize =
+    { width = 100, height = 100 }
+
 
 canonPosition : Position
-canonPosition = { x = 10, y = 10 }
+canonPosition =
+    { x = 50, y = 50 }
+
 
 emptyBoard : Board
-emptyBoard = 
+emptyBoard =
     { size = boardSize
     , sharks = []
     , octopuses = []
-    , canon = 
+    , canon =
         { direction = 25
         , position = canonPosition
         }
     }
-    
-
-
-
-init : ( Board, Cmd Msg )
-init = ( emptyBoard, Cmd.none )
-
-
-
-view : Board -> Html Msg
-view model =
-    div []
-        [ button [ onClick (Increment 2) ] [ text "+" ]
-        , text (toString model)
-        ]
 
 
 
@@ -97,21 +111,80 @@ update msg model =
 
 -- SUBSCRIPTIONS
 
+-- subscriptions : Model -> Sub Msg
+-- subscriptions model =
+--     AnimationFrame.times Tick
+
 
 subscriptions : Board -> Sub Msg
 subscriptions model =
     Sub.none
 
 
--- MAIN
+init : ( Board, Cmd Msg )
+init =
+    ( emptyBoard, Cmd.none )
 
 
-main : Program Never Board Msg
-main =
-    program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+-- view : Board -> Html Msg
+-- view model =
+--     div []
+--         [ button [ onClick (Increment 2) ] [ text "+" ]
+--         , text (toString model)
+--         ]
+
+
+view : Board -> Html Msg
+view board =
+    toHtml (collage 300 300 [canon board.canon, bullet { x=10, y=10} 45]) --(canon board.canon)
+
+
+
+
+offsetP : Position -> Float -> Float -> Float -> Position
+offsetP p w h a = 
+
+  let
+    m = rotation (degrees a)
+  in
+  
+    { x = p.x --- w/2
+    , y = p.y --- h/2
+    }
+
+
+-- canon: Canon -> Form
+-- canon c = Collage.text "This is the canon" 
+-- -- heartBeat : Float -> Element
+-- -- heartBeat delta =
+--     -- collage 300 300 [ heart |> scale (abs (sin (delta / 1000))) ]
+
+canon : Canon -> Form
+canon c =
+
+  let
+    w = 30
+    h = 6
+    p = offsetP c.position  w h c.direction
+  in
+
+    group
+    [ Collage.rect w h |> filled red |> move (p.x, p.y) |> rotate (degrees c.direction)
+    , Collage.circle 8 |> filled red |> move (c.position.x - 15, c.position.y)
+    ]
+
+bullet : Position -> Float -> Form
+bullet p angle =
+    group
+    [ Collage.rect 10 2 |> filled purple |> move (p.x, p.y) |> rotate (degrees angle)
+    ]
+
+heart : Form
+heart =
+    
+    group
+        [ ngon 4 50 |> filled red |> move ( 0, 0 )
+        , circle 36 |> filled red |> move ( 20, 20 )
+        , circle 36 |> filled red |> move ( -20, 20 )
+        ]
 
